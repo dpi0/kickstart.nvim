81baa21dd8fce6dae2f95eb86da0f56c8ca19660
dpi0 <dpi0.dev@proton.me>
2024-06-14 17:45:01 +0530

    changed font size neovide

--- lua/options.lua
+++ lua/options.lua
@@ -7,7 +7,7 @@ vim.opt.termguicolors = true
 vim.cmd 'set colorcolumn=+1'
 
 --- NEOVIDE
-vim.o.guifont = 'JetBrainsMono Nerd Font:h14'
+vim.o.guifont = 'JetBrainsMono Nerd Font:h13'
 vim.g.neovide_cursor_animation_length = 0.05
 vim.g.neovide_cursor_trail_size = 0.1
 vim.g.neovide_scroll_animation_length = 0.1
